150f97dbf7b4cc5867701e5b8d88496ed48796f9
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-08 20:50:26 +0900

    feat: donuTool 프로그램으로 추가한 북마크 폴더 분리 및 저장 #6

--- src/background/background.js
+++ src/background/background.js
@@ -38,14 +38,33 @@ chrome.runtime.onMessage.addListener((message) => {
   if (message.action === "bookmarkCurrentTab") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const tab = tabs[0];
-      chrome.bookmarks.create({
-        title: tab.title,
-        url: tab.url,
-      });
 
-      chrome.tabs.sendMessage(tab.id, {
-        action: "showBookmarkAlert",
-        title: tab.title,
+      const FOLDER_TITLE = "DonuTool 북마크 폴더";
+
+      chrome.bookmarks.search({ title: FOLDER_TITLE }, (results) => {
+        const folder = results.find((bookmarkNode) => bookmarkNode.title === FOLDER_TITLE && !bookmarkNode.url);
+        const parentId = folder?.id;
+
+        const createBookmark = (parentId) => {
+          chrome.bookmarks.create({
+            parentId,
+            title: tab.title,
+            url: tab.url,
+          });
+
+          chrome.tabs.sendMessage(tab.id, {
+            action: "showBookmarkAlert",
+            title: tab.title,
+          });
+        };
+
+        if (parentId) {
+          createBookmark(parentId);
+        } else {
+          chrome.bookmarks.create({ title: FOLDER_TITLE }, (newFolder) => {
+            createBookmark(newFolder.id);
+          });
+        }
       });
     });
   }
