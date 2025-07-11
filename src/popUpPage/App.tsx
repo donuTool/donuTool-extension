302605ec916ec6f24650f1c200e3332ec5eb9406
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-11 17:49:12 +0900

    feat: 게스트 모드 추가 #9

--- src/popUpPage/App.tsx
+++ src/popUpPage/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useThemeStore } from "@/stores/useThemeStore";
 import MainPage from "@/popUpPage/pages/MainPage";
 import SettingPage from "@/popUpPage/pages/SettingPage";
@@ -21,18 +22,36 @@ function App() {
     return () => media.removeEventListener("change", updateMode);
   }, []);
 
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-    chrome.storage?.local.set({ isDarkMode: !isDarkMode });
-  };
+  useEffect(() => {
+    chrome.storage?.local.get("user", (result) => {
+      if (result.user) {
+        navigate("/main");
+      }
+    });
+  }, []);
+
+  const navigate = useNavigate();
 
   const logInWithGoogle = async () => {
     try {
       const data = await googleLogin();
       console.log("Logged in: ", data.user);
+      chrome.storage?.local.set({ user: data.user });
+      navigate("/main");
     } catch (e) {
       console.error(e);
     }
+  };
+
+  const goToMainPage = () => {
+    chrome.storage?.local.set({ user: "guest" }, () => {
+      navigate("/main");
+    });
+  };
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    chrome.storage?.local.set({ isDarkMode: !isDarkMode });
   };
 
   return (
@@ -52,12 +71,20 @@ function App() {
               <h2 className="dark:text-donutool-text mb-4 text-3xl font-black text-neutral-600 transition duration-300 select-none">
                 DonuTool
               </h2>
-              <button
-                onClick={logInWithGoogle}
-                className="dark:bg-donutool-button dark:text-donutool-text flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-3.5 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
-              >
-                로그인
-              </button>
+              <div className="flex gap-2">
+                <button
+                  onClick={logInWithGoogle}
+                  className="dark:bg-donutool-button dark:text-donutool-text flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-3.5 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
+                >
+                  로그인
+                </button>
+                <button
+                  onClick={goToMainPage}
+                  className="dark:bg-donutool-button dark:text-donutool-text flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-3.5 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
+                >
+                  게스트
+                </button>
+              </div>
             </>
           }
         />
