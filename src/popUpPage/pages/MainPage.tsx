64f044f087b6026c4d2887b52034a75fc973975f
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-13 15:09:19 +0900

    feat: 메인페이지 진입 시 로그아웃 기능 제공 #9

--- src/popUpPage/pages/MainPage.tsx
+++ src/popUpPage/pages/MainPage.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import SetFullscreenButton from "@/popUpPage/components/SetFullscreenButton";
 import StartButton from "@/popUpPage/components/StartButton";
 import StopButton from "@/popUpPage/components/StopButton";
@@ -12,12 +13,30 @@ export default function MainPage() {
     );
   }
 
+  const navigate = useNavigate();
+
+  const logOutWithGoogle = () => {
+    chrome.storage?.local.remove(["user", "jwt"], () => {
+      if (chrome.runtime.lastError) {
+        alert(`Storage error: ${chrome.runtime.lastError.message}`);
+        return;
+      }
+      navigate("/");
+    });
+  };
+
   return (
     <>
       <Title />
       <SetFullscreenButton />
       <StartButton />
       <StopButton />
+      <button
+        onClick={logOutWithGoogle}
+        className="dark:bg-donutool-button dark:text-donutool-text absolute top-3 right-17 flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-3.5 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
+      >
+        로그아웃
+      </button>
       <GoToOptionButton />
     </>
   );
