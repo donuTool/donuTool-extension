647f600b6deda97036a9b32eea8a37bcf69e74bf
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-13 21:05:25 +0900

    chore: 초기 설정값 확장프로그램 실행 시점에 로컬로 저장하도록 위치 변경 #9

--- src/popUpPage/App.tsx
+++ src/popUpPage/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useThemeStore } from "@/stores/useThemeStore";
+import { useButtonStore } from "@/stores/useButtonStore";
 import Background from "@/popUpPage/components/Background";
 import LogInPage from "@/popUpPage/pages/LogInPage";
 import MainPage from "@/popUpPage/pages/MainPage";
@@ -10,7 +11,13 @@ import ThemeToggleButton from "@/popUpPage/components/buttons/ThemeToggleButton"
 
 function App() {
   const setIsDarkMode = useThemeStore((state) => state.setIsDarkMode);
+  const buttons = useButtonStore((state) => state.buttons);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    chrome.storage?.local.set({ buttonsSetting: buttons });
+  }, [buttons]);
 
   useEffect(() => {
     chrome.storage?.local.get("isDarkMode", (result) => {
