18489393ae8b00fdef8e99f618c62ec1fbaa6857
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-17 15:15:40 +0900

    design: 타이틀 폰트 추가 #10

--- src/popUpPage/components/Title.tsx
+++ src/popUpPage/components/Title.tsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (
-    <h2 className="dark:text-donutool-text mb-6 text-3xl font-black text-neutral-600 transition duration-300 select-none">
-      DonuTool
+    <h2 className="dark:text-donutool-text font-rix mb-6 text-3xl font-black text-neutral-600 transition duration-300 select-none">
+      donuTool
     </h2>
   );
 }
