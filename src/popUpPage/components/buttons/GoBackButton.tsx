032ffefe41341e8fe060c1f34a968ae0393c512f
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-28 20:34:24 +0900

    fix: 가독성을 위해 일부 버튼 텍스트에서 아이콘으로 대체 #11

--- src/popUpPage/components/buttons/GoBackButton.tsx
+++ src/popUpPage/components/buttons/GoBackButton.tsx
@@ -1,8 +1,7 @@
-import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import GoBackIcon from "@/assets/arrow-left.svg?react";
 
 export default function GoBackButton() {
-  const { t } = useTranslation();
   const navigate = useNavigate();
 
   const goBackToMainPage = () => {
@@ -12,9 +11,9 @@ export default function GoBackButton() {
   return (
     <button
       onClick={goBackToMainPage}
-      className="dark:bg-donutool-button dark:text-donutool-text absolute top-3 right-3 flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-3.5 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
+      className="dark:bg-donutool-button absolute top-3 right-3 flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-2 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
     >
-      {t("goBack")}
+      <GoBackIcon className="dark:stroke-donutool-text h-4 w-4 stroke-neutral-600" />
     </button>
   );
 }
--- src/popUpPage/components/buttons/GoToOptionButton.tsx
+++ src/popUpPage/components/buttons/GoToOptionButton.tsx
@@ -1,8 +1,7 @@
-import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import SettingIcon from "@/assets/settings.svg?react";
 
 export default function GoToOptionButton() {
-  const { t } = useTranslation();
   const navigate = useNavigate();
 
   const navigateToSettingPage = () => {
@@ -12,9 +11,9 @@ export default function GoToOptionButton() {
   return (
     <button
       onClick={navigateToSettingPage}
-      className="dark:bg-donutool-button dark:text-donutool-text absolute top-3 right-3 flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-3.5 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
+      className="dark:bg-donutool-button dark:text-donutool-text flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-2 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
     >
-      {t("setting")}
+      <SettingIcon className="dark:stroke-donutool-text h-4 w-4 stroke-neutral-600" />
     </button>
   );
 }
