2f576adddee08db31e427923be795a26a9bdf721
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-28 20:34:24 +0900

    fix: 가독성을 위해 일부 버튼 텍스트에서 아이콘으로 대체 #11

--- src/popUpPage/components/buttons/LogOutButton.tsx
+++ src/popUpPage/components/buttons/LogOutButton.tsx
@@ -1,8 +1,7 @@
-import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import LogOutIcon from "@/assets/log-out.svg?react";
 
 export default function LogOutButton() {
-  const { t } = useTranslation();
   const navigate = useNavigate();
 
   const logOutWithGoogle = () => {
@@ -28,9 +27,9 @@ export default function LogOutButton() {
   return (
     <button
       onClick={logOutWithGoogle}
-      className="dark:bg-donutool-button dark:text-donutool-text absolute top-3 right-17 flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-3.5 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
+      className="dark:bg-donutool-button dark:text-donutool-text flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-2 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
     >
-      {t("logout")}
+      <LogOutIcon className="dark:stroke-donutool-text h-4 w-4 stroke-neutral-600" />
     </button>
   );
 }
