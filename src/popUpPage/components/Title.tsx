edc1f49db8725babb17226aa564fdebacae48600
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-09-06 17:20:52 +0900

    refactor: 시작 가이드 컴퍼넌트화 및 OS 별로 분리 #13

--- src/popUpPage/components/Title.tsx
+++ src/popUpPage/components/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <h2 className="dark:text-donutool-text font-rix mb-6 text-3xl font-black text-neutral-600 transition duration-300 select-none">
+    <h2 className="dark:text-donutool-text font-rix my-5 text-3xl font-black text-neutral-600 transition duration-300 select-none">
       donuTool
     </h2>
   );
